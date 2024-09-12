44d58205a88c4537ccc90ba4671905b45d836c0b
peo_lsnascimento <peo_lsnascimento@uolinc.com>
2024-09-12 08:27:49 -0300

    Testes unitários do book

--- src/test/kotlin/com/bookstore/jpa/repositories/BookRepositoryTest.kt
+++ src/test/kotlin/com/bookstore/jpa/repositories/BookRepositoryTest.kt
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 
 @DataJpaTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class BookRepositoryTest {
 
     @Autowired
@@ -35,7 +37,7 @@ class BookRepositoryTest {
 
         author1 = AuthorModel(id = 1L, name = "Test Author 1")
         author2 = AuthorModel(id = 2L, name = "Test Author 2")
-        authorRepository.saveAll(listOf(author1, author2))
+        authorRepository.saveAll(mutableListOf(author1, author2))
     }
 
     @Test
@@ -45,7 +47,7 @@ class BookRepositoryTest {
         val book = BookModel(
             title = "Test Book",
             publisher = publisher,
-            authors = listOf(author1, author2),
+            authors = mutableListOf(author1, author2),
             review = null
         )
 
@@ -58,5 +60,40 @@ class BookRepositoryTest {
         assert(savedBook.publisher == publisher)
         assert(savedBook.authors.contains(author1))
         assert(savedBook.authors.contains(author2))
+    }
+
+    @Test
+    @DisplayName("Test Update Book")
+    fun testUpdateBook() {
+        // Cria o livro
+        val book = BookModel(
+            title = "Test Book",
+            publisher = publisher,
+            authors = mutableListOf(author1, author2),
+            review = null
+        )
+
+        // Salva o livro no banco de dados
+        val savedBook = bookRepository.save(book)
+
+        val newPublisher = PublisherModel(id = 10L, name = "Test New Publisher")
+        val savedNewPublisher = publisherRepository.save(newPublisher)
+
+        val updatedBook = savedBook.copy(
+            title = "Test Book Update",
+            publisher = savedNewPublisher,
+            authors = mutableListOf(author1, author2)
+        )
+
+        // Salva o livro atualizado
+        val savedUpdatedBook = bookRepository.save(updatedBook)
+
+        // Verifica se o livro foi atualizado corretamente
+        assert(savedUpdatedBook.id == savedBook.id)
+        assert(savedUpdatedBook.title == "Test Book Update")
+        assert(savedUpdatedBook.publisher == savedNewPublisher)
+        assert(savedUpdatedBook.authors.contains(author1))
+        assert(savedUpdatedBook.authors.contains(author2))
+        assert(savedUpdatedBook.publisher != publisher)
     }
 }
--- src/test/kotlin/com/bookstore/jpa/services/BookServiceTest.kt
+++ src/test/kotlin/com/bookstore/jpa/services/BookServiceTest.kt
@@ -2,6 +2,7 @@ package com.bookstore.jpa.services
 
 import com.bookstore.jpa.dto.BookDTO
 import com.bookstore.jpa.exceptions.AuthorNotFoundException
+import com.bookstore.jpa.exceptions.BookNotFoundException
 import com.bookstore.jpa.exceptions.PublisherNotFoundException
 import com.bookstore.jpa.models.AuthorModel
 import com.bookstore.jpa.models.BookModel
@@ -37,6 +38,7 @@ class BookServiceTest {
     @InjectMocks
     private lateinit var bookService: BookService
 
+    private lateinit var bookModel: BookModel
     private lateinit var bookDTO: BookDTO
     private lateinit var publisher: PublisherModel
     private lateinit var author1: AuthorModel
@@ -45,7 +47,7 @@ class BookServiceTest {
     @BeforeEach
     fun setUp() {
         bookDTO = BookDTO(
-            title = "Test Book",
+            title = "Test BookDTO",
             publisherId = 1L,
             authorIds = listOf(1L, 2L),
             review = "Great book!"
@@ -54,6 +56,13 @@ class BookServiceTest {
         publisher = PublisherModel(id = 1L, name = "Test Publisher")
         author1 = AuthorModel(id = 1L, name = "Test Author 1")
         author2 = AuthorModel(id = 2L, name = "Test Author 2")
+
+        bookModel = BookModel(
+            title = "Test BookModel",
+            publisher = publisher,
+            authors = mutableListOf(author1, author2),
+            review = null
+        )
     }
 
     @Test
@@ -61,13 +70,13 @@ class BookServiceTest {
     fun testSaveBook() {
         // Simula o comportamento de encontrar o Publisher e os Authors
         `when`(publisherRepository.findById(any())).thenReturn(Optional.of(publisher))
-        `when`(authorRepository.findAllById(any())).thenReturn(listOf(author1, author2))
+        `when`(authorRepository.findAllById(any())).thenReturn(mutableListOf(author1, author2))
 
         // Configura o BookModel para ser salvo
         val bookToSave = BookModel(
             title = bookDTO.title,
             publisher = publisher,
-            authors = listOf(author1, author2),
+            authors = mutableListOf(author1, author2),
             review = null
         )
 
@@ -88,8 +97,8 @@ class BookServiceTest {
     }
 
     @Test
-    @DisplayName("Test Publisher Not Found Exception")
-    fun testPublisherNotFoundException() {
+    @DisplayName("Test Publisher Not Found Exception in Save")
+    fun testPublisherNotFoundExceptionInSave() {
         // Simula o comportamento de não encontrar o Publisher
         `when`(publisherRepository.findById(any())).thenReturn(Optional.empty())
 
@@ -106,8 +115,8 @@ class BookServiceTest {
     }
 
     @Test
-    @DisplayName("Test Author Not Found Exception")
-    fun testAuthorNotFoundException() {
+    @DisplayName("Test Author Not Found Exception in Save")
+    fun testAuthorNotFoundExceptionInSave() {
         // Simula o comportamento de encontrar o Publisher, mas não encontrar todos os Authors
         `when`(publisherRepository.findById(any())).thenReturn(Optional.of(publisher))
         `when`(authorRepository.findAllById(any())).thenReturn(listOf(author1))
@@ -122,5 +131,138 @@ class BookServiceTest {
 
         // Verifica se o método save do repository não foi chamado
         verify(bookRepository, never()).save(any())
+    }
+
+    @Test
+    @DisplayName("Test Update Book")
+    fun testUpdateBook() {
+        // Configura os dados do livro existente
+        val existingBook = BookModel(
+            id = 1L,
+            title = "Old Test Book",
+            publisher = publisher,
+            authors = mutableListOf(author1, author2),
+            review = null
+        )
+
+        // Simula o comportamento de encontrar o livro existente
+        `when`(bookRepository.findById(any())).thenReturn(Optional.of(existingBook))
+
+        // Simula o comportamento de encontrar o Publisher e os Authors
+        `when`(publisherRepository.findById(1L)).thenReturn(Optional.of(publisher))
+        `when`(authorRepository.findAllById(listOf(1L, 2L))).thenReturn(mutableListOf(author1, author2))
+
+        // Configura o livro atualizado
+        val updatedBook = existingBook.copy(
+            title = bookDTO.title,
+            publisher = publisher,
+            authors = mutableListOf(author1, author2),
+            review = null
+        )
+
+        // Simula o comportamento do repository.save para salvar o livro atualizado
+        `when`(bookRepository.save(any(BookModel::class.java))).thenReturn(updatedBook)
+
+        // Chama o método update do BookService
+        val result = bookService.update(1L, bookDTO)
+
+        // Verifica se o livro foi atualizado corretamente
+        assert(result?.title == bookDTO.title)
+        assert(result?.publisher == publisher)
+        assert(result?.authors?.contains(author1) == true)
+        assert(result?.authors?.contains(author2) == true)
+
+        // Verifica se o book repository foi chamado para salvar o livro atualizado
+        verify(bookRepository, times(1)).save(any(BookModel::class.java))
+    }
+
+    @Test
+    @DisplayName("Test Book Not Found Exception in Update")
+    fun testBookNotFoundExceptionInUpdate() {
+        // Simula o comportamento de não encontrar o livro
+        `when`(bookRepository.findById(any())).thenReturn(Optional.empty())
+
+        // Captura a exceção lançada
+        val thrown = Assertions.assertThrows(BookNotFoundException::class.java) {
+            bookService.update(1L, bookDTO)
+        }
+
+        // Verifica se a exceção capturada contém a mensagem correta
+        Assertions.assertEquals("Book id 1 not found", thrown.message)
+
+        // Verifica se o método save do repository não foi chamado
+        verify(bookRepository, never()).save(any())
+    }
+
+    @Test
+    @DisplayName("Test Publisher Not Found Exception in Update")
+    fun testPublisherNotFoundExceptionInUpdate() {
+        // Simula o comportamento de não encontrar o Publisher
+        `when`(bookRepository.findById(anyLong())).thenReturn(Optional.of(bookModel))
+        `when`(publisherRepository.findById(anyLong())).thenReturn(Optional.empty())
+
+        // Captura a exceção lançada
+        val thrown = Assertions.assertThrows(PublisherNotFoundException::class.java) {
+            bookService.update(1L, bookDTO)
+        }
+
+        // Verifica se a exceção capturada contém a mensagem correta
+        Assertions.assertEquals("Publisher not found!", thrown.message)
+
+        // Verifica se o método save do repository não foi chamado
+        verify(bookRepository, never()).save(any())
+    }
+
+    @Test
+    @DisplayName("Test Author Not Found Exception in Update")
+    fun testAuthorNotFoundExceptionInUpdate() {
+        // Simula o comportamento de encontrar o Book e Publisher, mas não encontrar todos os Authors
+        `when`(bookRepository.findById(anyLong())).thenReturn(Optional.of(bookModel))
+        `when`(publisherRepository.findById(anyLong())).thenReturn(Optional.of(publisher))
+        `when`(authorRepository.findAllById(any())).thenReturn(listOf(author1)) // Simula apenas um autor encontrado
+
+        // Captura a exceção lançada
+        val thrown = Assertions.assertThrows(AuthorNotFoundException::class.java) {
+            bookService.update(1L, bookDTO)
+        }
+
+        // Verifica se a exceção capturada contém a mensagem correta
+        Assertions.assertEquals("Author not found!", thrown.message)
+
+        // Verifica se o método save do repository não foi chamado
+        verify(bookRepository, never()).save(any())
+    }
+
+    @Test
+    @DisplayName("Test Book Not Found Exception in Delete")
+    fun testBookNotFoundExceptionInDelete() {
+        // Simula o comportamento de não encontrar o livro
+        `when`(bookRepository.existsById(any())).thenReturn(false)
+
+        // Captura a exceção lançada
+        val thrown = Assertions.assertThrows(BookNotFoundException::class.java) {
+            bookService.deleteById(1L)
+        }
+
+        // Verifica se a exceção capturada contém a mensagem correta
+        Assertions.assertEquals("Book id 1 not found", thrown.message)
+
+        // Verifica se o método delete do repository não foi chamado
+        verify(bookRepository, never()).deleteById(any())
+    }
+
+    @Test
+    @DisplayName("Test Book Not Found Exception in FindById")
+    fun testBookNotFoundExceptionInFindById() {
+        // Simula o comportamento de não encontrar o livro
+        `when`(bookRepository.findById(any())).thenReturn(Optional.empty())
+
+        // Captura a exceção lançada
+        val thrown = Assertions.assertThrows(BookNotFoundException::class.java) {
+            bookService.findById(1L)
+        }
+
+        // Verifica se a exceção capturada contém a mensagem correta
+        Assertions.assertEquals("Book id 1 not found", thrown.message)
     }
 }
